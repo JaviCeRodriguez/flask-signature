e2eed90627d47392d07909979d37880634bdf9de
Javier Rodriguez <javicerodriguez@gmail.com>
2023-06-12 15:32:16 -0300

    add: very simple form for make signature

--- api/index.py
+++ api/index.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home(): 
+    if request.method == 'POST':
+        fullname = request.form.get('fullname')
+        rol = request.form.get('rol')
+        linkedin = request.form.get('linkedin')
+        cal = request.form.get('cal')
+        return render_template('result.html', fullname=fullname, rol=rol, linkedin=linkedin, cal=cal)
     return render_template('index.html')
 
 
-@app.route('/result', methods=['GET', 'POST'])
-def result(): 
-    return render_template('index.html')
+@app.route('/result')
+def result():
+    fullname = request.args.get('fullname')
+    rol = request.args.get('rol')
+    linkedin = request.args.get('linkedin')
+    cal = request.args.get('cal')
+    return render_template('results.html', fullname=fullname, rol=rol, linkedin=linkedin, cal=cal)
